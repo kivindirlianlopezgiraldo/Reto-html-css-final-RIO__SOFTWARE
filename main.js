8ca8c2020ae62fa4b1675307868613fab103ad24
kivindirlianlopezgiraldo <72659368+kivindirlianlopezgiraldo@users.noreply.github.com>
2022-01-31 20:57:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let efecto = document.getElementsByClassName("buscador");
+let efecto2 = document.getElementsByClassName("buscador__img");
+
+
+
+focus = function getFocus() {
+    document.getElementById("myTextField").focus();
+  }
+
+alert("hola")
+  
